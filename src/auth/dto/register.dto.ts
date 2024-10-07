da274e0053eea5ad1b9e8af1c0130c90ec7386ff
luisruro <lerubioromero@gmail.com>
2024-10-07 18:56:51 -0500

    feat: login.dto.ts adn register.dto.ts were created

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,25 @@
+import { Transform } from "class-transformer";
+import { IsEmail, IsNotEmpty, IsOptional, IsString, IsStrongPassword, MinLength } from "class-validator";
+
+export class RegisterDto {
+
+    @IsNotEmpty({message: 'Name is required and must be a string'})
+    @IsString()
+    @MinLength(3)
+    name: string;
+
+    @IsNotEmpty({message: 'Email is required and must be a valid email address'})
+    @IsString()
+    @IsEmail({},{ message: 'Please provide a valid email address' })
+    email:string;
+
+    @IsNotEmpty({message: 'Password is required and must be a valid password: at least 8 characters, 1 upper case, at least one digit from (0-9) and at least one special character'})
+    @IsString()
+    @IsStrongPassword({},{ message: 'Please provide a valid password: at least 8 characters, at least 1 upper case, at least one digit from (0-9) and at least one special character' })
+    @Transform(({value }) => value.trim())//For not accept only spaces in the password
+    password: string;
+
+    @IsOptional()
+    @IsString()
+    rol: string;
+}
