0528178e9457db7ae4062524d9d4a4d89f2ef58c
luisruro <lerubioromero@gmail.com>
2024-10-07 15:37:28 -0500

    feat: cors.ts and index.ts files into constants folder were created to configure cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CORS } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true}))
   const configService = app.get(ConfigService);
+  app.enableCors(CORS);
   app.setGlobalPrefix('api');
   await app.listen(configService.get('PORT'));
 }
