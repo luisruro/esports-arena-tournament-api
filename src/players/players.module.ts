1b35a1daf29545d04e3e67c2f0b9716af4b06774
luisruro <lerubioromero@gmail.com>
2024-10-07 16:09:20 -0500

    feat: Player entity was added into players.module.ts

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PlayersService } from './players.service';
 import { PlayersController } from './players.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Player } from './entities/player.entity';
 
 @Module({
   providers: [PlayersService],
-  controllers: [PlayersController]
+  controllers: [PlayersController],
+  imports: [TypeOrmModule.forFeature([Player])]
 })
 export class PlayersModule {}
