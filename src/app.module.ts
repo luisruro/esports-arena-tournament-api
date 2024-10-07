dff129c527d6b10ad80f25614cff86ff9cda2e28
luisruro <lerubioromero@gmail.com>
2024-10-07 15:22:53 -0500

    feat: app.module.ts was setted up to connect the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configurations from './common/config/configurations';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: []
+  imports: [
+    ConfigModule.forRoot({
+      load: [configurations],
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ]
 })
 export class AppModule {}
