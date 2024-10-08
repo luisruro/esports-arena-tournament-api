3e37bba6e8a4272904e1c71d10596df9c09ff070
luisruro <lerubioromero@gmail.com>
2024-10-07 19:16:20 -0500

    feat: @Post for login and register methods were created, @Get method was created but it's not working yet because @ActiveUser decorator has not been created yet just like ActiveUserInterface

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) { };
+
+    @Post('register')
+    register(@Body() registerDto: RegisterDto) {
+        return this.authService.register(registerDto);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    login(@Body() loginDto: LoginDto) {
+        return this.authService.login(loginDto);
+    }
+
+    @Get('profile')
+    //@Auth([Rol.USER])
+    profile(@ActiveUser() user: ActiveUserInterface) { //@ActiveUser() it's a customized decorator
+        console.log(user)
+        return this.authService.profile(user);
+    }
+}
+
