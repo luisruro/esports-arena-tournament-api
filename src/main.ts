f477969f73581db3fd05a017679378e369b4dbd6
luisruro <lerubioromero@gmail.com>
2024-10-07 21:34:18 -0500

    feat: config for swagger was added in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,24 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { CORS } from './common/constants';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true}))
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
   const configService = app.get(ConfigService);
   app.enableCors(CORS);
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
