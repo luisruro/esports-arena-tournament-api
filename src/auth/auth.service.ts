83aca151ed7a605e0e95c2e9cf3c03664b6441ba
luisruro <lerubioromero@gmail.com>
2024-10-07 19:06:26 -0500

    feat: register, login and profile methods were added in auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { RegisterDto } from './dto/register.dto';
+
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService
+    ) { };
+
+    async register({ password, email, name, rol }: RegisterDto) {
+        const userFound = await this.usersService.findOneByEmail(email);
+
+        if (userFound) {
+            throw new HttpException(`Email already registered ${userFound.email}`, HttpStatus.CONFLICT);
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await this.usersService.create({
+            name,
+            email,
+            password: hashedPassword,
+            rol
+        });
+
+        return {
+            message: `User created successfully`
+        }
+    }
+
+    async login({ email, password }: LoginDto) {
+        const user = await this.usersService.findOneByEmailWithPassword(email);
+
+        if (!user) {
+            throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordValid) {
+            throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+        }
+
+        const payload = { email: user.email, rol: user.rol };
+        const token = await this.jwtService.signAsync(payload)
+
+        return {
+            token: token,
+            email: user.email
+        };
+    }
+
+    async profile({ email, rol }: { email: string; rol: string }) {
+        return await this.usersService.findOneByEmail(email);
+      }
+}
+
