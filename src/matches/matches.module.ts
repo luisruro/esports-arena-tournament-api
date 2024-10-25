66c8ea9ec6ee74cdc6d48e65335cb2299ed75e35
luisruro <lerubioromero@gmail.com>
2024-10-25 17:40:53 -0500

    feat: imports in matches.module was setted up

--- src/matches/matches.module.ts
+++ src/matches/matches.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MatchesService } from './matches.service';
 import { MatchesController } from './matches.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Match } from './entities/match.entity';
+import { PlayersModule } from 'src/players/players.module';
+import { TournamentsModule } from 'src/tournaments/tournaments.module';
 
 @Module({
   providers: [MatchesService],
-  controllers: [MatchesController]
+  controllers: [MatchesController],
+  imports: [TypeOrmModule.forFeature([Match]), PlayersModule, TournamentsModule]
 })
 export class MatchesModule {}
